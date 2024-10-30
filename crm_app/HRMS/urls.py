a67717d318f31a8563e18d9ffd33755fb719bdea
Sunnyraj Choudhary <sunnyrajchoudhary321@gmail.com>
2024-10-30 11:02:02 +0530

    added global hr section by sunny and amit

--- crm_app/HRMS/urls.py
+++ crm_app/HRMS/urls.py
@@ -26,8 +26,7 @@ urlpatterns = [
     path('NewEmploye/', views.NewEmploye, name='NewEmploye'),
     path('emp_main/', views.emp_main, name='emp_main'),
     path('Employment/', views.Employments, name='Employment'),
-    path('Employe_position/', views.Employe_position, name='Employe_position'), 
-    
+    path('Employe_position/', views.Employe_position, name='Employe_position'),    
     path('lookup/', views.lookup, name='lookup'),
     path('NewEmployement/', views.NewEmployement, name='NewEmployement'),
     path('FindEmploye/', views.FindEmploye, name='FindEmploye'),
@@ -37,17 +36,36 @@ urlpatterns = [
     path('EditSalary/', views.EditSalary, name='EditSalary'), 
      
     # sunny hrms
-    path('Employee_app/', views.Employee_app, name='Employee_app'),
+    path('employement_appli/', views.employement_appli, name='employement_appli'),
     path('New_emp_app/', views.New_emp_app, name='New_emp_app'),
     path('resume/', views.resume, name='resume'),
     path('leave/', views.leave, name='leave'),
-    path('lookupempapp/', views.lookupempapp, name='lookupempapp'),
-    path('lookupempposi/', views.lookupempposi, name='lookupempposi'),
-    path('lookupparty/', views.lookupparty, name='lookupparty'),
-    path('Newresume/', views.Newresume, name='Newresume'),
-    path('lookpartyresume/', views.lookpartyresume, name='lookpartyresume'),
-    path('addempleave/', views.addempleave, name='addempleave'),
-    path('leaveappr/', views.leaveappr, name='leaveappr'),
+    path('lookup_emp_app/', views.lookup_emp_app, name='lookup_emp_app'),
+    path('lookup_emp_posi/', views.lookup_emp_posi, name='lookup_emp_posi'),
+    path('lookup_party/', views.lookup_party, name='lookup_party'),
+    path('New_resume/', views.New_resume, name='New_resume'),
+    path('lookup_party_resume/', views.lookup_party_resume, name='lookup_party_resume'),
+    path('add_emp_leave/', views.add_emp_leave, name='add_emp_leave'),
+    path('leave_approval/', views.leave_approval, name='leave_approval'),
+    
+    
+    # global HR by sunny and amit 
+    path('skill_types/', views.skill_types, name='skill_types'),
+    path('responsibility_types/', views.responsibility_types, name='responsibility_types'),
+    path('termination_reasons/', views.termination_reasons, name='termination_reasons'),
+    path('termination_types/', views.termination_types, name='termination_types'),
+    path('position_types/', views.position_types, name='position_types'),
+    path('emp_leave_types/', views.emp_leave_types, name='emp_leave_types'),
+    path('pay_grades/', views.pay_grades, name='pay_grades'),
+    path('job_interview_types/', views.job_interview_types, name='job_interview_types'),
+    path('tranning_class_types/', views.tranning_class_types, name='tranning_class_types'),
+    path('public_holiday/', views.public_holiday, name='public_holiday'),
+    path('EditPayGrade/', views.EditPayGrade, name='EditPayGrade'),
+    path('EmpLeaveReasonType/', views.EmpLeaveReasonType, name='EmpLeaveReasonType'),
+    path('emp_leave_types/', views.emp_leave_types, name='emp_leave_types'),
+    path('Edit_PositionTypes/', views.Edit_PositionTypes, name='Edit_PositionTypes'),
+    
+    
     
     # gannu hrms
     path('Skills/', views.Skills, name='Skills'),
